661af1ad9317a34a126fbaab5e801baf14fdd71c
bwcompton <bcompton@umass.edu>
2024-09-13 16:47:12 -0400

    Continue writing code

--- ecoConnect.quantiles.R
+++ ecoConnect.quantiles.R
@@ -1,8 +1,8 @@
-'ecoConnect.quantiles' <- function(n = 10000, acres  = c(1, 10, 100, 1000, 1e4, 1e5), best.pct = c(50, 25), 
+'ecoConnect.quantiles' <- function(n = 10000, acres  = c(1, 10, 100, 1000, 1e4, 1e5), best.pct = 0.5, 
                                    layers = c('forests', 'ridgetops', 'wetlands', 'floodplains' ), 
                                    server.names = c('Forest_fowet', 'Ridgetop', 'Nonfo_wet', 'LR_floodplain_forest'),
-                                   sourcepath = 'x:/LCC/GIS/Final/ecoRefugia/ecoConnect_final/ecoConnect_', 
-                                   threshold = 0.75) {
+                                   sourcepath = 'x:/LCC/GIS/Final/ecoRefugia/ecoConnect_final/', 
+                                   threshold = 0.25) {
    
    
    # Calculate percentiles of simulated parcels of ecoConnect layers and save files for ecoConnect.tool
@@ -11,12 +11,11 @@
    # Arguments:
    #   n                   number of samples
    #   acres               target parcel sizes in acres
-   #   best.pct            top percents of pseudoparcels for "best." A separate result file will be written for each provided
-   #                       value. We'll only use one of them in the end, either top 50% or top 25%.
+   #   best.pct            top percent of pseudoparcels for "best," as proportion 
    #   layers              source system names
    #   server.names        names used on server, to be written to result data frame
    #   sourcepath          path for source TIFFs
-   #   threshold           proportion of cells that must be non-missing to be counted as a good sample
+   #   threshold           maximum proportion of missing cells to be counted as a good sample
    #   
    # Source data (in sourcepath):
    #   shindex.tif         index grid for states and HUC8s, with nodata for all cells that should be masked out (marine, estaurine, freshwater tidal)
@@ -30,9 +29,85 @@
    #   sample_sizes_huc.txt
    #     tables of region/state name/huc ID by acres with sample sizes
    #     
+   #  realized.acres (printed to console)
+   #     a conversion from nominal acres to actual acerage of pseudoparcels. Use this in _________________ when interpolating percentiles for a given parcel.
+   #     
+   # Notes:
+   #    - we're reading the max block size if there are any nodata cells in block, which will be inefficient near edges
+   #      where smaller blocks have enough data but larger ones don't, but I'm opting for simplicity and clarity over speed
+   # 
    # B. Compton, 12 Sep 2024 (from ecoConnect.big.quantiles)
    
    
+   
+   library(terra)
+   library(lubridate)
+   
+   
+   ts <- Sys.time()
+   
+   'unpack' <- function(x)                                                             # unpack state and HUC ids
+      c(floor(x / 1000), x - floor(x / 1000) * 1000)
+   
+   
+   # come up with indices for each specified acerage 
+   cells.per.acre <- 4.496507136
+   w <- sqrt(acres * cells.per.acre)                                                   # convert acres to width in cells
+   w <- ifelse((floor(w) %% 2) != 0, floor(w), ceiling(w))                             # pick nearest odd width in cells
+   realized.acres <- w^2 / cells.per.acre                                              # realized acres - use in ______ for interpolating
+   cat('realized.acres <- c(', paste0(round(realized.acres, 4), collapse = ', '), ')\n', sep = '')    
+   rel.indices <- lapply(w, function(x) -floor(x / 2):floor(x / 2))                    # indices for each block size, relative to center
+   block.idx <- rel.indices[[length(block.indices)]]                                   # relitive indices for full block
+   
+   max.block <- max(w)                                                                 # maximum block size - this is what we'll always read
+   block.indices <- lapply(rel.indices, function(x) x + ceiling(max.block / 2))        # indices for each block size, absolute for max block 
+   
+   thresholds <- w^2 * threshold                                                       # NA thresholds for each block size 
+   
+   # read source data
+   shindex <- rast(paste0(sourcepath, 'shindex.tif'))                                  # combined state/HUC8 index and mask
+   lays <- lapply(layers, function(x) rast(paste0(sourcepath, 'ecoConnect_', x, '.tif')))
+   sinfo <- read.table(paste0(sourcepath, 'stateinfo.txt', sep = '\t', header = TRUE)) # we'll use these for sample size tables
+   hinfo <- read.table(paste0(sourcepath, 'hucinfo.txt', sep = '\t', header = TRUE))
+   
+   
+   # create results
+   statehuc <- data.frame(matrix(NA, 2, n))
+   names(statehuc) <- c('state', 'huc')            # state and HUC8 for each sample
+   z <- array(NA, dim = c(n, length(w), length(layers), 3, 2), 
+              dimnames = c('row', 'system', 'size', 'best'))                           # row x systems x acres x all/best
+   
+   
+   # gather samples
+   for(i in 1:n) {                                                                     # For each sample,
+      failure <- TRUE
+      got.data <- FALSE
+      while(failure) {                                                                 #    until we find a live one,
+         s <- round(runif(2) * dim(shindex)[1:2])                                      #    index of sample
+         x <- unlist(shindex[block.idx + s[1], block.idx + s[2]])                      #    read shindex for block
+         names(x) <- NULL        # ***************** remove names in prep fn, not here ***************
+         if(any(!is.na(x))) {                                                          #    If there are any data, continue
+            sh <- matrix(x, length(block.idx), length(block.idx), byrow = TRUE)        #       make a proper matrix of it
+            for(j in 1:length(acres)) {                                                #       for each block size,
+               if(sum(is.na(x[4:6, 4:6]) > thresholds[j]))                             #       bail if too many missing cells   
+                  next
+               if(!got.data) {                                                         #             if we don't have data yet,
+                  lay.vals <- lapply(lay, rast)                                        #                read all 4 layers
+                  got.data <- TRUE
+                  failure <- FALSE
+               }
+               
+               for(k in 1:length(layers)) {                                            #          for each layer,
+                  y <- lay.vals[[k]][block.indices[[j]], block.indices[[j]]]           #             pull out subblock
+                  y[is.na(x)] <- NA                                                    #             mask from shindex
+                  z[i, j, k, 1] <- mean(y, na.rm = TRUE)                               #             sample all values
+                  z[i, j, k, 2] <- mean(y[y >= quantile(y, best.pct)], na.rm = TRUE)   #             sample top best.pct
+               }
+            }
+         }
+         statehuc[i, ] <- unpack(sh[s[1], s[2]])                                       #    get state and HUC ids
+      }
+   }
    
    # come up with list of indices for each size. Need square root of acres in cells, pick closest odd number of cells
    # read shindex, stateinfo and hucinfo
@@ -55,6 +130,8 @@
    #                 take mean of top %
    #     if no success, pick another random cell
    #     
+
+   
    # Now loop for each best %
    #     take percentiles
    #     get sample size
@@ -65,43 +142,9 @@
    
    
    
+
    
-   
-   
-   
-   library(terra)
-   
-   cat('Running for ', acres, ' acres...\n', sep = '')
-   mask <- rast('x:/LCC/GIS/Work/Ancillary/Mask/phase5/mask.tif')
-   
-   cells <- acres * 4.496507136                                  # convert target to cells
-   w <- floor(c(cells, cells + 1))                               # width of square, rounded down and up
-   w <- w[w %% 2 == 1] - 1                                       # we want the odd width
-   r <- w / 2                                                    # radius of target square
-   k <- -r:r                                                     # add these to indices
-   l <- round(matrix(dim(mask)[1:2] - r * 2, n, 2, byrow = TRUE) * runif(n * 2) + r, 0)           # indices into n squares
-   
-   z <- data.frame(matrix(NA, 100, length(layers)))
-   names(z) <- server.names
-   
-   for(i in 1:length(layers)) {                                  # for each layer,
-      cat('Layer ', layers[i], '...', sep = '')
-      v <- rep(NA, n)
-      x <- rast(paste0(sourcepath, layers[i], '.tif'))            #    read raster
-      
-      for(j in 1:n) {                                             #    for each iteration,
-         y <- as.vector(x[l[j, 1] + k, l[j, 2] + k])[[1]]          #       sample
-         m <- as.vector(mask[l[j, 1] + k, l[j, 2] + k])[[1]]       #       and corresponding mask
-         y[is.na(y)] <- 0                                          #       nodata within mask should be zero for our purposes here
-         y[is.na(m) | (m != 1)] <- NA                                #       outside of mask is nodata
-         v[j] <- ifelse(sum(is.na(y)) > w ^ 2 / 2, NA, mean(y, na.rm = TRUE))    #       mean if half or less of area is NA  
-      }
-      
-      v <- v[!is.na(v)]                                                         #    drop NAs
-      cat('n = ', format(length(v), big.mark = ','), '\n', sep = '')
-      z[server.names[i]] <- quantile(v,  probs =  seq(0.01, 1, by = 0.01))      #    get percentiles
-   }
-   
-   saveRDS(z, f <- paste0(sourcepath, 'quantiles_', acres, '.RDS'))
+  # saveRDS(z, f <- paste0(sourcepath, 'quantiles_', ..., '.RDS'))
    cat('Results written to ', f, '\n', sep = '')
+   cat('Total time taken: ', seconds_to_period(round(as.duration(interval(ts, Sys.time())))), '\n', sep = '')
 }
