475edc941655dfdb2a54bc699d4e7e01243585db
bwcompton <bcompton@umass.edu>
2024-09-19 12:39:33 -0400

    Don't use blocks with nodata in center cell. This will speed things up and ensure we have a good state and HUC for each block.

--- ecoConnect.quantiles.R
+++ ecoConnect.quantiles.R
@@ -48,24 +48,24 @@
    # B. Compton, 12 Sep 2024 (from ecoConnect.big.quantiles)
    
    
-  #   n <<- n;acres<<-acres;best.pct <<- best.pct; layers <<- layers;server.names <<- server.names; sourcepath <<- sourcepath; threshold <<- threshold
-  #   return()
+   #   n <<- n;acres<<-acres;best.pct <<- best.pct; layers <<- layers;server.names <<- server.names; sourcepath <<- sourcepath; threshold <<- threshold
+   #   return()
    
    
    library(terra)
    library(lubridate)
    
    
-   'index.rast' <- function(x, s, indices) {                                              # index block of a raster object without blowing up on lower edges
-      i <- list(s[1] + indices, s[2] + indices)                                           # row and column indices, taking 1st col/row beyond lower edge
+   'index.rast' <- function(x, s, indices = 0) {                                             # index block of a raster object without blowing up on lower edges
+      i <- list(s[1] + indices, s[2] + indices)                                              # row and column indices, taking 1st col/row beyond lower edge
       z <- matrix(unlist(x[pmax(i[[1]], 1), pmax(i[[2]], 1)], use.names = FALSE), 
-                  length(indices), length(indices), byrow = TRUE)                         # read block and convert to matrix
-      z[i[[1]] < 1, ] <- NA                                                               # nuke rows off north edge, and columns off west edge
+                  length(indices), length(indices), byrow = TRUE)                            # read block and convert to matrix
+      z[i[[1]] < 1, ] <- NA                                                                  # nuke rows off north edge, and columns off west edge
       z[, i[[2]] < 1] <- NA
       z
    }
-  
-   'unpack' <- function(x)                                                                # unpack state and HUC ids
+   
+   'unpack' <- function(x)                                                                   # unpack state and HUC ids
       c(floor(x / 1000), x - floor(x / 1000) * 1000)
    
    
@@ -73,68 +73,70 @@
    ts <- Sys.time()
    
    # come up with indices for each specified acerage 
-   cells.per.acre <- 4.496507136                                                          # 1 acre = 4.5 cells
-   w <- sqrt(acres * cells.per.acre)                                                      # convert acres to width in cells
-   w <- ifelse((floor(w) %% 2) != 0, floor(w), ceiling(w))                                # pick nearest odd width in cells
-   realized.acres <- w^2 / cells.per.acre                                                 # realized acres - use for interpolation
+   cells.per.acre <- 4.496507136                                                             # 1 acre = 4.5 cells
+   w <- sqrt(acres * cells.per.acre)                                                         # convert acres to width in cells
+   w <- ifelse((floor(w) %% 2) != 0, floor(w), ceiling(w))                                   # pick nearest odd width in cells
+   realized.acres <- w^2 / cells.per.acre                                                    # realized acres - use for interpolation
    cat('realized.acres <- c(', paste0(round(realized.acres, 4), collapse = ', '),
        ') # Realized acres for interpolation\n', sep = '')    
-   rel.indices <- lapply(w, function(x) -floor(x / 2):floor(x / 2))                       # indices for each block size, relative to center
-   block.idx <- rel.indices[[length(rel.indices)]]                                        # relative indices for full block (we'll use this inside loop)
+   rel.indices <- lapply(w, function(x) -floor(x / 2):floor(x / 2))                          # indices for each block size, relative to center
+   block.idx <- rel.indices[[length(rel.indices)]]                                           # relative indices for full block (we'll use this inside loop)
    
-   max.block <- max(w)                                                                    # maximum block size - this is what we'll always read
-   block.indices <- lapply(rel.indices, function(x) x + ceiling(max.block / 2))           # indices for each block size, absolute for max block 
+   max.block <- max(w)                                                                       # maximum block size - this is what we'll always read
+   block.indices <- lapply(rel.indices, function(x) x + ceiling(max.block / 2))              # indices for each block size, absolute for max block 
    
-   thresholds <- w^2 * threshold                                                          # NA thresholds in cells for each block size 
+   thresholds <- w^2 * threshold                                                             # NA thresholds in cells for each block size 
    
    # read source data
-   shindex <- rast(paste0(sourcepath, 'shindex.tif'))                                     # combined state/HUC8 index and mask
+   shindex <- rast(paste0(sourcepath, 'shindex.tif'))                                        # combined state/HUC8 index and mask
    lays <- lapply(layers, function(x) rast(paste0(sourcepath, 'ecoConnect_', x, '.tif')))
-   sinfo <- read.table(paste0(sourcepath, 'stateinfo.txt'), sep = '\t', header = TRUE)    # we'll use these for sample size tables
+   sinfo <- read.table(paste0(sourcepath, 'stateinfo.txt'), sep = '\t', header = TRUE)       # we'll use these for sample size tables
    hinfo <- read.table(paste0(sourcepath, 'hucinfo.txt'), sep = '\t', header = TRUE)
    
    
    # create results
    statehuc <- data.frame(matrix(NA, n, 2))
    names(statehuc) <- c('state', 'huc')            # state and HUC8 for each sample
-   z <- array(NA, dim = c(n, length(w), length(layers), 2))                               # row x acres x systems x all/best
+   z <- array(NA, dim = c(n, length(w), length(layers), 2))                                  # row x acres x systems x all/best
    
    
    # gather samples
-   for(i in 1:n) {                                                                        # For each sample,
+   for(i in 1:n) {                                                                           # For each sample,
       cat('Trying ', i, '...\n', sep = '')
       success <- FALSE
-      while(!success) {                                                                   #    until we find a live one,
-         s <- round(runif(2) * dim(shindex)[1:2])                                         #    index of sample
-         cat('s = ', s[1], ', ', s[2], '\n', sep = '') 
-         sh <- index.rast(shindex, s, block.idx)                                          #    read shindex for block
-#         cat(c('   no data', '   got data')[any(!is.na(x)) + 1], '\n')
-         if(any(!is.na(sh))) {                                                            #    If there are any data, continue
-            got.layers <- FALSE
-            for(j in 1:length(acres)) {                                                   #       for each block size,
-#               cat('   ', acres[j], ' acres\n')
-               if(sum(is.na(sh[block.indices[[j]], block.indices[[j]]])) > thresholds[j]) #       bail if too many missing cells   
-               {
-#                  cat('      Too many missing   ********************\n')
-                  next
+      while(!success) {                                                                      #    until we find a live one,
+         s <- round(runif(2) * dim(shindex)[1:2])                                            #    index of sample
+         # cat('s = ', s[1], ', ', s[2], '\n', sep = '') 
+         if(!is.na(index.rast(shindex, s, 0))) {                                             #    if focal cell has data,
+            sh <- index.rast(shindex, s, block.idx)                                          #       read shindex for block
+            #         cat(c('   no data', '   got data')[any(!is.na(x)) + 1], '\n')
+            if(any(!is.na(sh))) {                                                            #       If there are any data, continue
+               got.layers <- FALSE
+               for(j in 1:length(acres)) {                                                   #          for each block size,
+                  #               cat('   ', acres[j], ' acres\n')
+                  if(sum(is.na(sh[block.indices[[j]], block.indices[[j]]])) > thresholds[j]) #          bail if too many missing cells   
+                  {
+                     #                  cat('      Too many missing   ********************\n')
+                     next
+                  }
+                  #               cat('      Is okay...\n')
+                  if(!got.layers) {                                                          #                if we don't have data yet,
+                     #                  cat('      Reading layers...\n')
+                     lay.vals <- lapply(lays, function(x) index.rast(x, s, block.idx))       #                   read current block of all 4 ecoConnect layers as matrices
+                     got.layers <- TRUE
+                     success <- TRUE
+                  }
+                  
+                  for(k in 1:length(layers)) {                                               #             for each layer,
+                     y <- lay.vals[[k]][block.indices[[j]], block.indices[[j]]]              #                pull out subblock
+                     y[is.na(sh[block.indices[[j]], block.indices[[j]]])] <- NA              #                mask from shindex
+                     z[i, j, k, 1] <- mean(y, na.rm = TRUE)                                  #                sample all values
+                     z[i, j, k, 2] <- mean(y[y >= quantile(y, best.pct, na.rm = TRUE)], 
+                                           na.rm = TRUE)                                     #                sample top best.pct
+                  }
                }
-#               cat('      Is okay...\n')
-               if(!got.layers) {                                                          #             if we don't have data yet,
-#                  cat('      Reading layers...\n')
-                  lay.vals <- lapply(lays, function(x) index.rast(x, s, block.idx))       #                read current block of all 4 ecoConnect layers as matrices
-                  got.layers <- TRUE
-                  success <- TRUE
-               }
-               
-               for(k in 1:length(layers)) {                                               #          for each layer,
-                  y <- lay.vals[[k]][block.indices[[j]], block.indices[[j]]]              #             pull out subblock
-                  y[is.na(sh[block.indices[[j]], block.indices[[j]]])] <- NA              #             mask from shindex
-                  z[i, j, k, 1] <- mean(y, na.rm = TRUE)                                  #             sample all values
-                  z[i, j, k, 2] <- mean(y[y >= quantile(y, best.pct, na.rm = TRUE)], 
-                                        na.rm = TRUE)                                     #             sample top best.pct
-               }
+               statehuc[i, ] <- unpack(sh[floor(max.block / 2), floor(max.block / 2)])       #       get state and HUC ids
             }
-            statehuc[i, ] <- unpack(sh[floor(max.block / 2), floor(max.block / 2)])       #    get state and HUC ids                ********* use majority, not centroid **********
          }
       }
    }
