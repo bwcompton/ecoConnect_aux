c13d0537c0d0d881e946c399ae143eb3930fdaf3
bwcompton <bcompton@umass.edu>
2024-10-29 15:04:42 -0400

    Script to run sample.for.quantiles for about 24 hours, 1e7 samples

---
+++ run_sample_quantiles.R
@@ -0,0 +1,9 @@
+# sample quantiles
+# run sample.for.quantiles a few times
+# B. Compton, 29 Oct 2024
+
+
+
+source('X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.R')
+#sample.for.quantiles(n = 1e6, postfix = 'new')   # 2 hrs expected
+sample.for.quantiles(n = 1e7, postfix = 'new')   # 24 hrs expected
