066ddfb004d08190e617adf6b574fd54da0ced5d
bwcompton <bcompton@umass.edu>
2024-10-29 15:35:54 -0400

    Add cascading timer to run_sample_quantiles

--- run_sample_quantiles.R
+++ run_sample_quantiles.R
@@ -5,5 +5,10 @@
 
 
 source('X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.R')
+
+i <- 2
+m <- 7                            # this is how many minutes it takes to read data
+Sys.sleep(60 * m * i)            # sleep to give other runs a chance to load data. Set i to iteration number
+
 #sample.for.quantiles(n = 1e6, postfix = 'new')   # 2 hrs expected
 sample.for.quantiles(n = 1e7, postfix = 'new')   # 24 hrs expected
