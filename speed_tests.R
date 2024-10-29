b2d6bbf997a1d9baed2f6c703863461da852d4fc
bwcompton <bcompton@umass.edu>
2024-10-29 14:49:08 -0400

    Missing source() in get.quantiles, junky speed_tests script

--- speed_tests.R
+++ speed_tests.R
@@ -1,20 +1,25 @@
 source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.R")
-source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday.R")
-source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday2.R")
-source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday3.R")
+# source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday.R")
+# source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday2.R")
+# source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.thursday3.R")
 source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.1.R")
 source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.2.R")
 source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.3.R")
 source("X:/LCC/Code/ecoConnect_aux/sample.for.quantiles.4.R")
 
-# sample.for.quantiles(n = 10, postfix = 'testxx')
+# sample.for.quantiles.1(n = 10, postfix = 'testxx')
+# saveRDS(saved, 'x:/temp/saved.RDS')
+# saved <- readRDS('x:/temp/saved.RDS')
 
 
-sample.for.quantiles.thursday2(n = 1e4, postfix = 'test_thurs2')
-sample.for.quantiles.thursday3(n = 1e4, postfix = 'test_thurs3')
-gc()
-sample.for.quantiles.thursday(n = 1e4, postfix = 'test_thurs')
-sample.for.quantiles.thursday(n = 1e5, postfix = 'test_thurs')
+# sample.for.quantiles.thursday2(n = 1e4, postfix = 'test_thurs2')
+# sample.for.quantiles.thursday3(n = 1e4, postfix = 'test_thurs3')
+# gc()
+# sample.for.quantiles.thursday(n = 1e4, postfix = 'test_thurs')
+# sample.for.quantiles.thursday(n = 1e5, postfix = 'test_thurs')
+# 
+# 
+saved <- saved.small
 cat('----------\n')
 gc()
 sample.for.quantiles.1(n = 1e4, postfix = 'test1')
@@ -24,6 +29,8 @@ gc()
 sample.for.quantiles.2(n = 1e4, postfix = 'test2')
 sample.for.quantiles.2(n = 1e5, postfix = 'test2')
 cat('----------\n')
+
+saved <- saved.big
 gc()
 sample.for.quantiles.3(n = 1e4, postfix = 'test3')
 sample.for.quantiles.3(n = 1e5, postfix = 'test3')
