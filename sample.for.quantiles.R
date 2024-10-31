9c11e0a503cd474aa5ea21a303f208bb841a8cdb
bwcompton <bcompton@umass.edu>
2024-10-31 14:05:38 -0400

    Do time reporting for each loop

--- sample.for.quantiles.R
+++ sample.for.quantiles.R
@@ -67,13 +67,7 @@
    library(collapse)
    
    launch <- now()
-   ts <- Sys.time()
    
-   
-   handlers(global = TRUE)                                                                      # for progress bar
-   handlers('rstudio')
-   skip <- 100                                                                                   # report progress every skipth iteration
-   pb <- progressor(n / skip)
    
    if(!all(acres == sort(acres)))
       stop('acres must be in ascending order (and correspond with n.factor, if supplied')
@@ -122,7 +116,7 @@
       list(state = as.vector(floor(x / 1000)), huc = as.vector(x - floor(x / 1000) * 1000))
    
    
-
+   
    # read source data
    shindex <- read.tiff(paste0(sourcepath, 'shindex.tif'))                                      # combined state/HUC8 index and mask
    lays <- lapply(layers, function(x) read.tiff(paste0(sourcepath, 'ecoConnect_', x, '.tif')))  # ecoConnect layers
@@ -135,116 +129,123 @@
    repeat {
       if((loop <- loop + 1) > 1 & !repeat.forever)
          break
-   cat('Loop ', loop, ' of ', n, ' samples', 
-       ifelse(repeat.forever, ' (repeating forever)', ''), '...\n', sep = '')
-   
-   # come up with indices for each specified acreage 
-   cells.per.acre <- 4.496507136                                                                # 1 acre = 4.5 cells
-   idx <- list(acres = acres)
-   idx$w <- sqrt(idx$acres * cells.per.acre)                                                    # convert acres to width in cells
-   idx$w <- ifelse((floor(idx$w) %% 2) != 0, floor(idx$w), ceiling(idx$w))                      # pick nearest odd width in cells
-   idx$thresholds <- idx$w^2 * threshold                                                        # NA thresholds in cells for each block size 
-   realized.acres <- idx$w^2 / cells.per.acre                                                   # realized acres - use for interpolation
-   
-   idx$rel.indices <- lapply(idx$w, function(x) -floor(x / 2):floor(x / 2))                     # indices for each block size, relative to center
-   idx$n <- rep(n, length(acres))
-   idx$n.factor <- n.factor
-   
-   idx <- set.up.indices(idx, n, n.factor)                                                      # set up indices; to be amended when we drop block sizes based on n.factor                                                                 
-   
-  
-   
-   
-   # create results
-   statehuc <- data.frame(matrix(NA, n, 2))
-   names(statehuc) <- c('state', 'huc')            # state and HUC8 for each sample
-   z <- array(NA, dim = c(n, length(acres), length(layers), 2))                                 # row x acres x systems x all/best
-   size <- dim(shindex)[1:2] - 1
-   cat('Gathering ', format(n, scientific = FALSE, big.mark = ','), ' samples...\n', sep = '')
-   
-   # gather samples
-   for(i in 1:n) {                                                                              # For each sample,
-      success <- FALSE
-      while(!success) {                                                                         #    until we find a live one,
-         s <- round(runif(2) * size + 1)                                                        #    index of sample
-         if(!is.na(index.block(shindex, s, 0))) {                                               #    if focal cell has data,
-            sh <- index.block(shindex, s, idx$block.idx)                                        #       read shindex for block
-            if(any(!is.na(sh))) {                                                               #       If there are any data, continue
-               got.layers <- FALSE
-               for(j in 1:length(idx$acres)) {                                                  #          for each block size,
-                  if(sum(is.na(sh[idx$block.indices[[j]], idx$block.indices[[j]]])) > 
-                     idx$thresholds[j])                                                         #          bail if too many missing cells   
-                     next
-                  if(!got.layers) {                                                             #                if we don't have data yet,
-                     lay.vals <- lapply(lays, function(x) index.block(x, s, idx$block.idx))     #                   read current block of all 4 ecoConnect layers as matrices
-                     got.layers <- TRUE
-                     success <- TRUE
+      cat('Loop ', loop, ' of ', n, ' samples', 
+          ifelse(repeat.forever, ' (repeating forever)', ''), '...\n', sep = '')
+      
+      handlers(global = TRUE)                                                                   # for progress bar
+      handlers('rstudio')
+      skip <- 100                                                                               # report progress every skipth iteration
+      pb <- progressor(n / skip)
+      ts <- Sys.time()
+      
+      
+      # come up with indices for each specified acreage 
+      cells.per.acre <- 4.496507136                                                             # 1 acre = 4.5 cells
+      idx <- list(acres = acres)
+      idx$w <- sqrt(idx$acres * cells.per.acre)                                                 # convert acres to width in cells
+      idx$w <- ifelse((floor(idx$w) %% 2) != 0, floor(idx$w), ceiling(idx$w))                   # pick nearest odd width in cells
+      idx$thresholds <- idx$w^2 * threshold                                                     # NA thresholds in cells for each block size 
+      realized.acres <- idx$w^2 / cells.per.acre                                                # realized acres - use for interpolation
+      
+      idx$rel.indices <- lapply(idx$w, function(x) -floor(x / 2):floor(x / 2))                  # indices for each block size, relative to center
+      idx$n <- rep(n, length(acres))
+      idx$n.factor <- n.factor
+      
+      idx <- set.up.indices(idx, n, n.factor)                                                   # set up indices; to be amended when we drop block sizes based on n.factor                                                                 
+      
+      
+      
+      
+      # create results
+      statehuc <- data.frame(matrix(NA, n, 2))
+      names(statehuc) <- c('state', 'huc')            # state and HUC8 for each sample
+      z <- array(NA, dim = c(n, length(acres), length(layers), 2))                              # row x acres x systems x all/best
+      size <- dim(shindex)[1:2] - 1
+      cat('Gathering ', format(n, scientific = FALSE, big.mark = ','), ' samples...\n', sep = '')
+      
+      # gather samples
+      for(i in 1:n) {                                                                           # For each sample,
+         success <- FALSE
+         while(!success) {                                                                      #    until we find a live one,
+            s <- round(runif(2) * size + 1)                                                     #    index of sample
+            if(!is.na(index.block(shindex, s, 0))) {                                            #    if focal cell has data,
+               sh <- index.block(shindex, s, idx$block.idx)                                     #       read shindex for block
+               if(any(!is.na(sh))) {                                                            #       If there are any data, continue
+                  got.layers <- FALSE
+                  for(j in 1:length(idx$acres)) {                                               #          for each block size,
+                     if(sum(is.na(sh[idx$block.indices[[j]], idx$block.indices[[j]]])) > 
+                        idx$thresholds[j])                                                      #          bail if too many missing cells   
+                        next
+                     if(!got.layers) {                                                          #                if we don't have data yet,
+                        lay.vals <- lapply(lays, function(x) index.block(x, s, idx$block.idx))  #                   read current block of all 4 ecoConnect layers as matrices
+                        got.layers <- TRUE
+                        success <- TRUE
+                     }
+                     
+                     for(k in 1:length(layers)) {                                               #             for each layer,
+                        y <- lay.vals[[k]][idx$block.indices[[j]], idx$block.indices[[j]]]      #                pull out subblock
+                        y[is.na(sh[idx$block.indices[[j]], idx$block.indices[[j]]])] <- NA      #                mask from shindex
+                        z[i, j, k, 1] <- mean(y, na.rm = TRUE)                                  #                sample all values
+                        z[i, j, k, 2] <- mean(y[y >= fnth(as.vector(y), n = best.pct, na.rm = TRUE)],
+                                              na.rm = TRUE)                                     #                sample top best.pct
+                     }
                   }
                   
-                  for(k in 1:length(layers)) {                                                  #             for each layer,
-                     y <- lay.vals[[k]][idx$block.indices[[j]], idx$block.indices[[j]]]         #                pull out subblock
-                     y[is.na(sh[idx$block.indices[[j]], idx$block.indices[[j]]])] <- NA         #                mask from shindex
-                     z[i, j, k, 1] <- mean(y, na.rm = TRUE)                                     #                sample all values
-                     z[i, j, k, 2] <- mean(y[y >= fnth(as.vector(y), n = best.pct, na.rm = TRUE)],
-                                           na.rm = TRUE)                                        #                sample top best.pct
+                  if((max(sh, na.rm = TRUE) - min(sh, na.rm = TRUE)) < 1)                       #       get most common state and HUC ids
+                     statehuc[i, ] <- unpack(sh[1])
+                  else {
+                     shu <- unpack(sh)
+                     statehuc[i, ] <- c(fmode(shu$state, na.rm = TRUE), fmode(shu$huc, na.rm = TRUE))
                   }
+                  
+                  
+                  if(i %% skip == 0)                                                            #       update progress bar every nth iteration
+                     pb()
                }
-               
-               if((max(sh, na.rm = TRUE) - min(sh, na.rm = TRUE)) < 1)                          #       get most common state and HUC ids
-                  statehuc[i, ] <- unpack(sh[1])
-               else {
-                  shu <- unpack(sh)
-                  statehuc[i, ] <- c(fmode(shu$state, na.rm = TRUE), fmode(shu$huc, na.rm = TRUE))
-               }
-               
-               
-               if(i %% skip == 0)                                                               #       update progress bar every nth iteration
-                  pb()
             }
          }
+         if(i >= idx$next.drop)                                                                 #    if it's time to drop samples,
+            if(is.null(idx <- set.up.indices(idx, n, n.factor, i + 1)))                         #       drop 'em
+               break
       }
-      if(i >= idx$next.drop)                                                                    #    if it's time to drop samples,
-         if(is.null(idx <- set.up.indices(idx, n, n.factor, i + 1)))                            #       drop 'em
-            break
-   }
-   
-   
-   
-   elapsed <- format(seconds_to_period(round(as.duration(interval(ts, Sys.time())))))
-   
-   # Save intermediate results z, statehuc, acres, layers 
-   x <- list(samples = z, statehuc = statehuc, acres = acres, layers = layers, realized.acres = realized.acres)
-   if(postfix != '')
-      p <- paste0('_', postfix)
-   p <- paste0(p, '_', gsub('[: ]', '_', substr(now(), 1, 16)))
-   saveRDS(x, f <- paste0(sourcepath, 'ec_samples', p, '.RDS'))                                 # write samples and associated variables to RDS
-   
-   fileConnect <- file(filename <- paste0(sourcepath, 'metadata', p, '.txt'))                   # write metadata file
-   x <- (paste0('Metadata for ecoConnect.quantiles run launched ', launch))
-   x <- c(x, paste0('Total time taken: ', elapsed, '\n'))
-   x <- c(x, paste0('realized.acres <- c(', paste0(round(realized.acres, 4), collapse = ', '),
-                    ') # Realized acres for interpolation\n'))
-   x <- c(x, 'Arguments:\n')
-   writeLines(x, fileConnect)
-   close(fileConnect)
-   
-   sink(filename, append = TRUE)
-   print(call.args)
-   sink()
-   
-   
-   ss <- rep(NA, length(acres))                                                                 # print sample sizes
-   for(j in 1:length(acres)) 
-      ss[j] <- sum(!is.na(z[, j, 1, 1]))
-   names(ss) <- acres
-   cat('\nSample sizes:\n')
-   print(ss)
-   cat('\n')
-   
-   
-   cat('Results written to ', f, '. To finish off, run\n\n', sep = '')
-   cat('   get.quantiles(\'', sourcepath, '\', \'', postfix, '\')\n\n', sep = '')
-   cat('This will gather samples from all sample files in ', sourcepath, '*', postfix, '.RDS\n', sep = '')
-   cat('Total time taken: ', elapsed, '\n', sep = '')
+      
+      
+      
+      elapsed <- format(seconds_to_period(round(as.duration(interval(ts, Sys.time())))))
+      
+      # Save intermediate results z, statehuc, acres, layers 
+      x <- list(samples = z, statehuc = statehuc, acres = acres, layers = layers, realized.acres = realized.acres)
+      if(postfix != '')
+         p <- paste0('_', postfix)
+      p <- paste0(p, '_', gsub('[: ]', '_', substr(now(), 1, 16)))
+      saveRDS(x, f <- paste0(sourcepath, 'ec_samples', p, '.RDS'))                              # write samples and associated variables to RDS
+      
+      fileConnect <- file(filename <- paste0(sourcepath, 'metadata', p, '.txt'))                # write metadata file
+      x <- (paste0('Metadata for ecoConnect.quantiles run launched ', launch))
+      x <- c(x, paste0('Total time taken: ', elapsed, '\n'))
+      x <- c(x, paste0('realized.acres <- c(', paste0(round(realized.acres, 4), collapse = ', '),
+                       ') # Realized acres for interpolation\n'))
+      x <- c(x, 'Arguments:\n')
+      writeLines(x, fileConnect)
+      close(fileConnect)
+      
+      sink(filename, append = TRUE)
+      print(call.args)
+      sink()
+      
+      
+      ss <- rep(NA, length(acres))                                                              # print sample sizes
+      for(j in 1:length(acres)) 
+         ss[j] <- sum(!is.na(z[, j, 1, 1]))
+      names(ss) <- acres
+      cat('\nSample sizes:\n')
+      print(ss)
+      cat('\n')
+      
+      
+      cat('Results written to ', f, '. To finish off, run\n\n', sep = '')
+      cat('   get.quantiles(\'', sourcepath, '\', \'', postfix, '\')\n\n', sep = '')
+      cat('This will gather samples from all sample files in ', sourcepath, '*', postfix, '.RDS\n', sep = '')
+      cat('Total time taken: ', elapsed, '\n', sep = '')
    }
 }
