c4671524b00ab11732cb32dc9a58baae4d8c6d9f
bwcompton <bcompton@umass.edu>
2025-01-08 12:02:02 -0500

    Use bridgeprob instead of flow for now; I'll switch it once we know more

--- rcl_crossings.R
+++ rcl_crossings.R
@@ -1,9 +1,10 @@
-'rcl_crossings' <- function(flow, traffic, source = 'x:/LCC/GIS/Final/NER/caps_phase5/tables/crossings.txt') {
+'rcl_crossings' <- function(flow, bridgeprob = 0.25, traffic = 1000, source = 'x:/LCC/GIS/Final/NER/caps_phase5/tables/crossings.txt') {
    
    
    # Screen crossings table for Regional Critical Linkages, given flow and traffic thresholds
    # Arguments:
-   #     flow           threshold of flow accumulation (column d8accum) as a surrogate for bridgeprob
+   #   ( flow           threshold of flow accumulation (column d8accum) as a surrogate for bridgeprob - later for this )
+   #     bridgeprob     I'm using bridgeprob directly at this early stage; we'll replace it with flow later
    #     traffic        threshold of traffic (column ADT)
    # Results:
    #     rcl_crossings.txt    screened file, in same directory as source
@@ -14,7 +15,8 @@
    x <- read.table(source, sep = '\t', header = TRUE)
    cat('Source has ', dim(x)[1], ' cases\n')
    
-   x <- x[x$d8accum >= flow & x$ADT >= traffic, ]
+ #  x <- x[x$d8accum >= flow & x$ADT >= traffic, ]
+   x <- x[x$bridgeprob >= bridgeprob & x$ADT >= traffic, ]
    f <- paste0(dirname(source), '/rcl_', basename(source))
    write.table(x, f, row.names = FALSE)
    
